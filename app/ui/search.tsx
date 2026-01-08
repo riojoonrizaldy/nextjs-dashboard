7cb688583942ec35e7364e189262abe95efc20f2
riojoonrizaldy <rizaldyrio07@gmail.com>
2026-01-08 22:31:43 +0700

    server action

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,22 +2,28 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams,usePathname,useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 export default function Search({ placeholder }: { placeholder: string }) {
-
+  
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const {replace} = useRouter();
+  
 
-  function handleSearch(term: string) {
-    const params = new URLSearchParams(searchParams);
+  const handleSearch = useDebouncedCallback((term) => {
+  console.log(`Searching... ${term}`);
+ 
+  const params = new URLSearchParams(searchParams);
+  params.set('page', '1'); // Reset to first page on new search
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
